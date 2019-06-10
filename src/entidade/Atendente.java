4dfa7beb3d1df00f8e1c9eb176b95e8b0059d285
André Farias <andrefariasti@gmail.com>
2019-06-10 16:48:58 -0300

    Ajuste de comentario

--- src/entidade/Atendente.java
+++ src/entidade/Atendente.java
@@ -33,24 +33,7 @@ public class Atendente extends Pessoa implements GerenciaAtendenteService {
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         frame.setLocationRelativeTo( null );
         frame.setVisible( true );
-		
-		//		String nome = JOptionPane.showInputDialog("Digite o nome do Atendente:");
-//		String telefone = JOptionPane.showInputDialog("Digite o Telefone:");
-//		String dtNascimento = JOptionPane.showInputDialog("Digite a data de nascimento");
-//		String cpf = JOptionPane.showInputDialog("Digite o numero de CPF");
-//		Atendente atendente = new Atendente();
-//		atendente.setNome(nome);
-//		Random gerador  = new Random();
-//		Long matricula = gerador.nextLong();
-//		atendente.setMatricula(matricula.toString());
-//		atendente.setTelefone(telefone);
-//		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
-//		Date dataFormatada = formato.parse(dtNascimento);
-//		atendente.setDtNascimento(dataFormatada);
-//		atendente.setCPF(cpf);
-//		
-//		JOptionPane.showMessageDialog(null, "Atendente Salvo!", null, 1);
-//		
-		return null;
+
+        return null;
 	}
 }
